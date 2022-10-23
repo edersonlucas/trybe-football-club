93aee3338667962887108c503b40f859a266c790
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-23 00:13:15 -0300

    feat: Cria o middleware de erro

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import ErrorMiddleware from './middlewares/ErrorMiddleware';
 
 class App {
   public app: express.Express;
@@ -22,6 +23,7 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
+    this.app.use(ErrorMiddleware.error);
   }
 
   public start(PORT: string | number):void {
