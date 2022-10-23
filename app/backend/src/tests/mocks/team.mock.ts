513b6505c5bb47aa230812f8148440b0686e6553
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-23 14:19:52 -0300

    test: Cria os mocks para o teste de teams

---
+++ app/backend/src/tests/mocks/team.mock.ts
@@ -0,0 +1,19 @@
+export const findAllMock = [
+  {
+    "id": 1,
+    "teamName": "Avaí/Kindermann"
+  },
+  {
+    "id": 2,
+    "teamName": "Bahia"
+  },
+  {
+    "id": 3,
+    "teamName": "Botafogo"
+  }
+]
+
+export const findOneMock = {
+  "id": 5,
+  "teamName": "Cruzeiro"
+}
