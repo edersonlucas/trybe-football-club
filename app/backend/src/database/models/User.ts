cbd9723712228b00a545cb246ab35ccb79b3a18d
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-23 00:14:21 -0300

    feat: Cria a model user

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,43 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.';
+
+export default class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: STRING,
+      allowNull: false,
+    },
+    role: {
+      type: STRING,
+      allowNull: false,
+    },
+    email: {
+      type: STRING,
+      allowNull: false,
+    },
+    password: {
+      type: STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'users',
+    timestamps: false,
+  },
+);
