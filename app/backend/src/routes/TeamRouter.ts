fb6068b8c9ffb7a2705d889a6d0ab9979cceb23e
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-23 14:18:54 -0300

    feat: Cria a rota GET /teams/id

--- app/backend/src/routes/TeamRouter.ts
+++ app/backend/src/routes/TeamRouter.ts
@@ -4,6 +4,7 @@ import TeamController from '../controllers/TeamController';
 const teamRouter = Router();
 
 teamRouter
+  .get('/teams/:id', new TeamController().getById)
   .get('/teams', new TeamController().getAll);
 
 export default teamRouter;
