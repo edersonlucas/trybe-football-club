4140e713bbbd364c1a03ad35375dc20abe3d1851
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-24 22:10:17 -0300

    feat: Cria a rota POST /matches

--- app/backend/src/routes/MatchRouter.ts
+++ app/backend/src/routes/MatchRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import AuthMiddleware from '../middlewares/AuthMiddleware';
 import MatchController from '../controllers/MatchController';
 
 const matchRouter = Router();
 
 matchRouter
   .get('/matches', new MatchController().getAll)
+  .post('/matches', AuthMiddleware.auth, new MatchController().create)
 export default matchRouter;
