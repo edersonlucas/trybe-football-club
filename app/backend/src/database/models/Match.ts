1e6074bb3e7316d9f8832fd635f342a6201154a1
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-24 22:00:30 -0300

    feat: Cria o model Match

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,53 @@
+import { INTEGER, BOOLEAN, Model } from 'sequelize';
+import db from '.';
+import Team from './Team';
+
+export default class Match extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Match.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    homeTeam: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    homeTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeam: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    inProgress: {
+      type: BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'matches',
+    timestamps: false,
+  },
+);
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
+Team.hasMany(Match, { foreignKey: 'id' });
