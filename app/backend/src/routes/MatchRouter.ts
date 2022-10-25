e15f892af832559754244cc7dbd4d761f84b6168
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-24 22:11:23 -0300

    feat: Cria a rota PATCH /matches/id

--- app/backend/src/routes/MatchRouter.ts
+++ app/backend/src/routes/MatchRouter.ts
@@ -8,4 +8,6 @@ matchRouter
   .get('/matches', new MatchController().getAll)
   .post('/matches', AuthMiddleware.auth, new MatchController().create)
   .patch('/matches/:id/finish', AuthMiddleware.auth, new MatchController().finish)
+  .patch('/matches/:id', AuthMiddleware.auth, new MatchController().update);
+
 export default matchRouter;
