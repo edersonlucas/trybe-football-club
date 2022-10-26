58d030cb7c535d77e4752f3a53a98d729d90cf08
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-25 22:23:04 -0300

    feat: Cria a rota GET /leaderboard

--- app/backend/src/routes/ScoreboardRouter.ts
+++ app/backend/src/routes/ScoreboardRouter.ts
@@ -6,4 +6,6 @@ const scoreboardRouter = Router();
 scoreboardRouter
   .get('/leaderboard/home', new ScoreboardController().getAllHome)
   .get('/leaderboard/away', new ScoreboardController().getAllAway)
+  .get('/leaderboard/', new ScoreboardController().getAll);
+
 export default scoreboardRouter;
